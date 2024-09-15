3eae04ee7ad4635b66784c34808344fb8293db5f
AYTC <aytacalir@gmail.com>
2024-09-15 20:56:29 +0300

    qq

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,12 +3,16 @@ import React from "react";
 const Login = () => {
   return (
     <div className="loginDiv">
-      <div className="h-[500px] w-11/12 sm:w-[475px] bg-orange-600 rounded-[20px] p-5 flex flex-col justify-center text-center" >
-      <div>
-        <span></span>
-        <h1 className="text-[22px] sm:-[32px]">
+      <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col justify-center text-center" >
+      <div className="flex justify-center items-center mt-2 gap-2">
+        <span className="w-[6px] h-[39px] bg-yellow-200"  ></span>
+        <h1 className="text-[22px] sm:-[32px] font-montserrat font-700 uppercase  ">
           Clarus Store
         </h1>
+      </div>
+      <div className="my-2">
+        <h3 className="font-montserrat font-600 text-[22px]">SING IN</h3>
+        <h6 className="font-montserrat text-labelColor texy-label mt-1">Enter your credetentials to access your account</h6>
       </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -11,6 +11,18 @@ module.exports = {
       },
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        label: [
+          "14px",
+          {
+            lineHeight: "normal",
+            fontWeight: "500",
+          },
+        ],
+      },
+      boxShadow: {
+        login: "2px 5px 10px 0px rgba(0, 0, 0, 0.1)",
       },
     },
   },
