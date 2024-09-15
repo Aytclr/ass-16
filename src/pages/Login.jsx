e7c2d143924f88908da5cc0162c11fbbaf1f9112
AYTC <aytacalir@gmail.com>
2024-09-15 20:44:29 +0300

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="loginDiv">
+      <div className="h-[500px] w-11/12 sm:w-[475px] bg-orange-600 rounded-[20px] p-5 flex flex-col justify-center text-center" >
+      <div>
+        <span></span>
+        <h1 className="text-[22px] sm:-[32px]">
+          Clarus Store
+        </h1>
+      </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#FEAF00",
+        navbarColor: "#F2EAE1",
+        labelColor: "#6C6C6C",
+        white: "#fff",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
